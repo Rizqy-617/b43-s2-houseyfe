5203363ea292cf6e30dd2fd3d259d2faccee0fae
Rizqy-617 <rizqyandriansyah2205@gmail.com>
2023-01-20 07:38:44 +0700

    signup receive a value

--- src/Route.js
+++ src/Route.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Signin from './components/Button/Signin';
 
 function RoutePage() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<Signin/>} />
       </Routes>
     </BrowserRouter>
   )
--- src/components/Button/Signup/index.jsx
+++ src/components/Button/Signup/index.jsx
@@ -1,11 +1,30 @@
 import React, {useState} from 'react';
 import { Button, Modal, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import styles from './index.module.css';
 
 export default function Signup() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow =() => setShow(true);
+
+  const [input, setInput] = useState({
+    fullname: "",
+    username: "",
+    email: "",
+    password: "",
+    list: "",
+    gender: "",
+    phone: "",
+    address: "",
+  });
+
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem("user", JSON.stringify(input));
+    navigate("/")
+  };
 
   return (
     <>
@@ -19,43 +38,59 @@ export default function Signup() {
           <Form>
             <Form.Group className='mb-3'>
               <Form.Label className={styles.labelbold}>Full Name</Form.Label>
-              <Form.Control type='text' placeholder='Your Full Name' autoFocus/>
+              <Form.Control type='text' placeholder='Your Full Name' name='fullname' value={input.fullname} onChange={(e) => setInput({
+                ...input,[e.target.name]: e.target.value,
+              })} autoFocus/>
             </Form.Group>
             <Form.Group className='mb-3'>
               <Form.Label className={styles.labelbold}>Username</Form.Label>
-              <Form.Control type='text' placeholder='Your Username' autoFocus/>
+              <Form.Control type='text' placeholder='Your Username' name='username' value={input.username} onChange={(e) => setInput({
+                ...input,[e.target.name]: e.target.value,
+              })} autoFocus/>
             </Form.Group>
             <Form.Group className='mb-3'>
               <Form.Label className={styles.labelbold}>Email</Form.Label>
-              <Form.Control type='email' placeholder='Your Email' autoFocus/>
+              <Form.Control type='email' placeholder='Your Email' name='email' value={input.email} onChange={(e) => setInput({
+                ...input,[e.target.name]: e.target.value,
+              })} autoFocus/>
             </Form.Group>
             <Form.Group className='mb-3'>
               <Form.Label className={styles.labelbold}>Password</Form.Label>
-              <Form.Control type='password' placeholder='Your Password' autoFocus/>
+              <Form.Control type='password' placeholder='Your Password' name='password' value={input.password} onChange={(e) => setInput({
+                ...input,[e.target.name]: e.target.value,
+              })} autoFocus/>
             </Form.Group>
             <Form.Group className='mb-3'>
               <Form.Label className={styles.labelbold}>List As</Form.Label>
-              <Form.Select autoFocus>
+              <Form.Select name='list' value={input.list} onChange={(e) => setInput({
+                ...input,[e.target.name]: e.target.value,
+              })} autoFocus>
                 <option>Tenant</option>
                 <option>Owner</option>
               </Form.Select>
             </Form.Group>
             <Form.Group className='mb-3'>
               <Form.Label className={styles.labelbold}>Gender</Form.Label>
-              <Form.Select autoFocus>
+              <Form.Select name='gender' value={input.gender} onChange={(e) => setInput({
+                ...input,[e.target.name]: e.target.value,
+              })} autoFocus>
                 <option>Laki-laki</option>
                 <option>Perempuan</option>
               </Form.Select>
             </Form.Group>
             <Form.Group className='mb-3'>
               <Form.Label className={styles.labelbold}>Phone</Form.Label>
-              <Form.Control type='text' placeholder='username' autoFocus/>
+              <Form.Control type='text' placeholder='Your Phone Number' name='phone' value={input.phone} onChange={(e) => setInput({
+                ...input,[e.target.name]: e.target.value,
+              })} autoFocus/>
             </Form.Group>
             <Form.Group className='mb-3'>
               <Form.Label className={styles.labelbold}>Address</Form.Label>
-              <Form.Control as='textarea' style={{height: '100px'}} autoFocus/>
+              <Form.Control as='textarea' style={{height: '100px'}} name='address' value={input.address} onChange={(e) => setInput({
+                ...input,[e.target.name]: e.target.value,
+              })} autoFocus/>
             </Form.Group>
-            <Button className={styles.fullbtn} type='button'>Sign Up</Button>
+            <Button className={styles.fullbtn} type='button' onClick={handleSubmit}>Sign Up</Button>
           </Form>
         </Modal.Body>
       </Modal>
