de454fa0d4ff6ed25a82135218ab607f64d73a04
Rizqy-617 <rizqyandriansyah2205@gmail.com>
2023-01-23 04:04:39 +0700

    Sign Out now working

--- src/components/Button/Dropdown/index.jsx
+++ src/components/Button/Dropdown/index.jsx
@@ -6,9 +6,15 @@ import {TbReceipt2} from "react-icons/tb";
 import {RiLogoutBoxRLine} from "react-icons/ri";
 import { Dropdown, Image } from 'react-bootstrap';
 import PP from '../../../assets/icons/Il-Dottore.jpg'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 
 export default function Droplist() {
+  const navigate = useNavigate();
+  const removeLoginData = () => {
+    localStorage.removeItem("isSignin");
+    alert("Goodbye")
+    navigate("/")
+  }
   return (
     <Dropdown align="end">
       <Dropdown.Toggle className={style.toggle}>
@@ -25,8 +31,8 @@ export default function Droplist() {
         History
         </Dropdown.Item>
         <Dropdown.Divider />
-        <Dropdown.Item className={style.textBold}><RiLogoutBoxRLine size={32} className={style.cg} />
-        Logout
+        <Dropdown.Item className={style.textBold} onClick={ removeLoginData } o><RiLogoutBoxRLine size={32} className={style.cg}/>
+        Signout
         </Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
